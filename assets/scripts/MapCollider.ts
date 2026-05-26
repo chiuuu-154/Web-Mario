dff56cde8c3ceb8c2e3d619ba615d7f721f29f47
113062308 <b.c.chihyi@gmail.com>
2026-05-26 14:20:47 +0800

    coin mushroom question block

--- assets/scripts/MapCollider.ts
+++ assets/scripts/MapCollider.ts
@@ -10,15 +10,17 @@ export default class MapCollider extends cc.Component {
         let tiledMap = this.node.getComponent(cc.TiledMap);
         let tileSize = tiledMap.getTileSize();
 
-        // 1. 生成普通地板 (讀取 top 圖層，放入 default 群組)
+        // 1. 生成普通地板 (維持不變)
         this.createCollidersForLayer(tiledMap, "top", "default", tileSize);
         this.createCollidersForLayer(tiledMap, "toponBlack", "default", tileSize);
         this.createCollidersForLayer(tiledMap, "flagBase", "default", tileSize);
 
-        // 2. 生成單向穿透平台 (讀取 oneway 圖層，放入 oneway 群組)
+        // 2. 生成單向穿透平台 (維持不變)
         this.createCollidersForLayer(tiledMap, "oneway", "oneway", tileSize);
 
-        this.replaceTilesWithPrefabs(tiledMap, "question", this.questionPrefab, tileSize);
+        // 🌟 3. 生成問號磚塊 (改成兩行，多傳一個數字進去：0 代表金幣，1 代表蘑菇)
+        this.replaceTilesWithPrefabs(tiledMap, "question_coin", this.questionPrefab, tileSize, 0);
+        this.replaceTilesWithPrefabs(tiledMap, "question_mushroom", this.questionPrefab, tileSize, 1);
     }
 
     // 寫一個共用的生成函數，這樣以後你要加冰塊、岩漿圖層都很方便
@@ -52,34 +54,34 @@ export default class MapCollider extends cc.Component {
         }
     }
 
-    replaceTilesWithPrefabs(tiledMap: cc.TiledMap, layerName: string, prefab: cc.Prefab, tileSize: cc.Size) {
-        if (!prefab) return; // 如果沒掛藍圖就不執行
+    // 🌟 函數名稱後面多加了一個 itemType: number
+    replaceTilesWithPrefabs(tiledMap: cc.TiledMap, layerName: string, prefab: cc.Prefab, tileSize: cc.Size, itemType: number) {
+        if (!prefab) return; 
         
         let layer = tiledMap.getLayer(layerName);
         if (!layer) return; 
         
         let layerSize = layer.getLayerSize();
 
-        // 掃描整張地圖的 questions 圖層
         for (let x = 0; x < layerSize.width; x++) {
             for (let y = 0; y < layerSize.height; y++) {
                 
-                // 如果這個格子有畫東西 (不是空格子)
                 if (layer.getTileGIDAt(x, y) !== 0) {
                     
-                    // 1. 算出它的精準世界座標 (跟碰撞體的算法一模一樣)
                     let posX = x * tileSize.width + tileSize.width / 2;
                     let posY = (layerSize.height - y - 1) * tileSize.height + tileSize.height / 2;
                     
-                    // 2. 用藍圖印出一個真實的問號方塊！
                     let block = cc.instantiate(prefab);
                     
-                    // 3. 把它放在算好的位置上，並成為地圖的孩子 (這樣才會跟著地圖縮放)
+                    // 🌟 核心魔法：強制把生出來的磚塊，設定成我們要的道具類型！
+                    let script = block.getComponent("QuestionBlock");
+                    if (script) {
+                        script.blockType = itemType; 
+                    }
+                    
                     block.setPosition(posX, posY);
                     layer.node.addChild(block);
                     
-                    // 4. 【神不知鬼不覺】把 TiledMap 上原本的圖案「擦掉」 (GID 設為 0)
-                    // 這樣畫面就不會有兩個方塊重疊了！
                     layer.setTileGIDAt(0, x, y);
                 }
             }
